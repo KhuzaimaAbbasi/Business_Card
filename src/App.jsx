f6aedb7bd819dc3ea0399d1388cb36262aea9736
Hazimuka <u2019341@giki.edu.pk>
2023-11-05 17:08:18 +0500

    Calling all components using a seperate functions and importing it into main

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Info from './info'
+import About from './About'
+import Interests from './Interests'
+import Footer from './Footer'
+
+
+export default function App()
+{
+return (
+    <div>
+    <Info />
+    <About/>
+    <Interests/>
+    <Footer/>
+</div>
+
+
+)
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import { FaInstagram, FaTwitter } from "react-icons/fa"
-import { FaFacebook } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
-import Info from './info'
-import About from './About'
-import Interests from './Interests'
-import Footer from './Footer'
+import App from './App'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Info />
-    <About/>
-    <Interests/>
-    <Footer/>
+ <App/>
  </React.StrictMode>,
 
 )
